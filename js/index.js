3b16eb42e6f6fa4f5702c1f0967930550cf5baed
Maribel Duran <maribelduranucm@gmail.com>
2017-01-05 21:11:44 -0800

    Updated class names for single_pens.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-document.getElementById("cover-overlay").addEventListener("mouseover", function(){
+var cover_overlay = document.getElementsByClassName("cover-overlay");
+
+for (var i = 0; i < cover_overlay.length; i++) {
+    cover_overlay[i].addEventListener("mouseover", function(){
  console.log("Currently MouseOver")
   this.style.opacity = 0.8;
         });
-
-document.getElementById("cover-overlay").addEventListener("mouseleave", function(){
+  
+   cover_overlay[i].addEventListener("mouseleave", function(){
  console.log("Currently MouseOver")
   this.style.opacity = 0;
         });
+}
+
