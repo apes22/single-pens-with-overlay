3aff7df043678e2ad54679fc0113f2d9919a85f5
Maribel Duran <maribelduranucm@gmail.com>
2017-01-11 00:14:36 -0800

    Edited html class names and updated style sheet. Removed comments from js script file.

--- js/index.js
+++ js/index.js
@@ -2,13 +2,11 @@ var cover_overlay = document.getElementsByClassName("cover-overlay");
 
 for (var i = 0; i < cover_overlay.length; i++) {
     cover_overlay[i].addEventListener("mouseover", function(){
- console.log("Currently MouseOver")
-  this.style.opacity = 0.8;
-        });
+    	this.style.opacity = 0.8;
+    });
   
    cover_overlay[i].addEventListener("mouseleave", function(){
- console.log("Currently MouseOver")
-  this.style.opacity = 0;
-        });
+   	this.style.opacity = 0;
+   });
 }
 
